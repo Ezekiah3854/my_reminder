806a7ba4f3989275398d05b856c60a6f1d430622
zack <nyagwayaezekiah@gmail.com>
2025-01-31 14:12:23 +0300

    commit changes

--- website/views.py
+++ website/views.py
@@ -29,6 +29,7 @@ def delete_note():
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
+            flash('Note deleted!', category='error')
     return jsonify({})
 
 
